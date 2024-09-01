d4ca68e2ccd9906d486c5fca03254510c00ec803
medcloud <medcloud.pk@gmail.com>
2024-09-01 15:05:01 +0500

    commit   test

--- client/src/Components/Posts.jsx
+++ client/src/Components/Posts.jsx
@@ -5,39 +5,43 @@ import axios from "axios";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
-  const [ loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-useEffect(()=>{
-  const fetchPosts = async () => {
-    setLoading(true);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
 
-    try{
-       
-     const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts`)
-       setPosts(response?.data)
+      try {
+        const response = await axios.get(
+          `https://mern-blog-kappa-one.vercel.app/posts`
+        );
+        setPosts(response?.data);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
 
-    }catch(err){
-       console.log(err)
-    }
-            setLoading(false)
+  if (loading) {
+    return <Loader />;
   }
-  fetchPosts();
-},[])
-
-
-
-   if(loading){
-    return  <Loader/>
-   }
-
-
 
   return (
     <section className="posts">
       {posts.length > 0 ? (
         <div className="container posts_container">
           {posts.map(
-            ({ _id: id, thumbnail, title, description, category, creator,createdAt }) => (
+            ({
+              _id: id,
+              thumbnail,
+              title,
+              description,
+              category,
+              creator,
+              createdAt,
+            }) => (
               <Postitem
                 key={id}
                 postID={id}
