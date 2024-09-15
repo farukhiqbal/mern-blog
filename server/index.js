946baf9d95c34d46203144c9d2479525a04e5942
medcloud <medcloud.pk@gmail.com>
2024-09-15 12:57:47 +0500

    server commit

--- server/index.js
+++ server/index.js
@@ -11,17 +11,11 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "https://futurestack.vercel.app/" }));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://futurestack.vercel.app/");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+app.use(cors({
+  origin: 'https://futurestack.vercel.app',
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+}));
 
 app.get("/", (req, res) => {
   res.send("server is running ");
