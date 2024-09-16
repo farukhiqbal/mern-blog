dc184ef2020dc0fdbf28a11746ce5158528c6e3d
medcloud <medcloud.pk@gmail.com>
2024-09-16 12:01:17 +0500

    localhost

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const { connect } = require("mongoose");
 require("dotenv").config();
 const upload = require("express-fileupload");
-
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
@@ -11,18 +10,20 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://localhost:3000/',
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE , PATCH",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("server is running ");
 });
 
 app.use(upload());
-app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("/api/uploads", express.static(__dirname + "/uploads"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
