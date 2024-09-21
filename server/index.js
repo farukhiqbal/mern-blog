e6b6bb0e8f7d6c332bc97e02de29f010a3ac000e
medcloud <medcloud.pk@gmail.com>
2024-09-21 19:04:11 +0500

    server commti

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://futurestack.vercel.app/",
     methods: "GET,POST,PUT,DELETE , PATCH",
     credentials: true,
   })
